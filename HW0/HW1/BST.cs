8c188f79650be6e2bce6a3da592cc3fb49e338dd
Nathan Bunge <nathan.bunge@wsu.edu>
2023-01-24 11:28:08 -0800

    added user input

---
+++ HW0/HW1/BST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    internal class BST
+    {
+
+        //insert number into bst, don't allow duplicate
+        void insert(int n)
+        {
+            return;
+        }
+
+        //print tree in sorted order
+        void printTree()
+        {
+
+            return;
+        }
+
+        //get number of items in the tree
+        int numItems()
+        {
+
+            return 0;
+        }
+
+        //get number of levels in tree
+        int numLevels()
+        {
+
+            return 0;
+        }
+    }
+}
--- HW0/HW1/Program.cs
+++ HW0/HW1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Nathan Bunge
+// 1-26-2023
+// Cpts 321 HW1
+
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter list of numbers seperated by spaces");
+
+
+//get user input
+string i = Console.ReadLine();
+
+//parse string into integers
+string[] vs = i.Split(' ');
+
+int[] convertedItems = Array.ConvertAll<string, int>(vs, int.Parse);
+
+foreach (int item in convertedItems)
+{
+    Console.WriteLine(item);
+}
