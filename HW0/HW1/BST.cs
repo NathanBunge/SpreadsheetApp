2f5b77399b4d6978fd5fc9fab3530b9bce6f275b
Nathan Bunge <nathan.bunge@wsu.edu>
2023-01-25 16:45:25 -0800

    added BST methods

--- HW0/HW1/BST.cs
+++ HW0/HW1/BST.cs
@@ -6,34 +6,119 @@ using System.Threading.Tasks;
 
 namespace HW1
 {
-    internal class BST
+    public class Node
     {
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+        public int Val { get; set; }
+    }
+    public class BST
+    {
+        private Node? Root { get; set; }
 
         //insert number into bst, don't allow duplicate
-        void insert(int n)
+        public void insert(int newVal)
         {
+            //make new node
+            Node newNode = new()
+            {
+                Val = newVal
+            };
+
+            //if tree is empty, set to root
+            if (this.Root == null)
+            {
+                this.Root = newNode;
+                return;
+            }
+                
+            //make node pointers
+            Node cur = Root;
+            Node next = Root;
+
+            //itterate a temp to end of a branch, have another temp follow 1 behind
+            while (next != null)
+            {
+                cur = next;
+                if (newVal < next.Val) 
+                    next = next.Left;
+                else if (newVal > next.Val) 
+                    next = next.Right;
+
+                //if not less or greater, then it is a duplicate.
+                else
+                {
+                    return;
+                }
+
+            }
+
+
+            if (newVal < cur.Val)
+                cur.Left = newNode;
+            else
+                cur.Right = newNode;
+
+
             return;
         }
+    
+
 
         //print tree in sorted order
-        void printTree()
+        public void printTreeOrdered()
         {
+            preOrderPrint(this.Root);
+        }
+
+        private void preOrderPrint(Node n)
+        {
+            if (n is null)
+            {
+                return;
+            }
+            //print left side
+            preOrderPrint(n.Left);
+
+            //print current
+            Console.WriteLine(n.Val);
+
+            //print right side
+            preOrderPrint(n.Right);
 
             return;
         }
 
         //get number of items in the tree
-        int numItems()
+        public int numItems()
         {
 
-            return 0;
+            return getNumItems(this.Root);
+        }
+
+        private int getNumItems(Node n)
+        {
+            if (n is null)
+            {
+                return 0;
+            }
+            return getNumItems(n.Left) + getNumItems(n.Right) + 1;
         }
 
         //get number of levels in tree
-        int numLevels()
+        public int numLevels()
         {
 
-            return 0;
+            return getNumLevels(this.Root);
+        }
+
+        private int getNumLevels(Node n)
+        {
+            if (n is null)
+            {
+                return 0;
+            }
+            return Math.Max(getNumItems(n.Left), getNumItems(n.Right)) + 1;
         }
     }
 }
--- HW0/HW1/Program.cs
+++ HW0/HW1/Program.cs
@@ -19,3 +19,16 @@ foreach (int item in convertedItems)
 {
     Console.WriteLine(item);
 }
+
+
+HW1.BST tree = new HW1.BST();
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+tree.insert(4);
+
+
+tree.printTreeOrdered();
+
+Console.WriteLine(tree.numItems().ToString());
+Console.WriteLine(tree.numLevels().ToString());
