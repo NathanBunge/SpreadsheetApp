e925d96444e5a45e77ce1d4921ebfcfe30ea0c87
Nathan Bunge <nathan.bunge@wsu.edu>
2023-03-10 14:15:51 -0800

    Some stylecoup

--- HW0/SpreadsheetEngine/ExpressionTree.cs
+++ HW0/SpreadsheetEngine/ExpressionTree.cs
@@ -1,20 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpreadsheetEngine
+﻿namespace SpreadsheetEngine
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
+    /// <summary>
+    /// Class to contain expression tree and nodes.
+    /// </summary>
     public class ExpressionTree
     {
-        private readonly ExpressionNode _root;
+        private readonly ExpressionNode root;
         private Dictionary<string, double> variableDict = new Dictionary<string, double>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionTree"/> class.
+        /// Constructor for expression tree.
+        /// </summary>
+        /// <param name="expression">expression to add.</param>
         public ExpressionTree(string expression)
         {
-            _root = Compile(expression);
+            this.root = this.Compile(expression);
+        }
+
+        /// <summary>
+        /// sets variable name, adding to dict.
+        /// </summary>
+        /// <param name="variableName">name of variable to set.</param>
+        /// <param name="variableValue">value of variable to set.</param>
+        public void SetVariable(string variableName, double variableValue)
+        {
+            this.variableDict.Add(variableName, variableValue);
+        }
+
+        /// <summary>
+        /// Evaluate the tree.
+        /// </summary>
+        /// <returns>value of evaluation</returns>
+        public double Evaluate()
+        {
+            return this.root.Evaluate();
         }
 
         private ExpressionNode Compile(string expression)
@@ -27,80 +53,119 @@ namespace SpreadsheetEngine
                 return new NumericNode(value);
             }
 
+            // If states with a letter, lookup in dictionary
+            // TODO: 
+
             string leftside = expression.Substring(0,1);
             string rightside = expression.Substring(2);
 
             char op = expression[1];
 
-
             if (op == '+')
             {
-                return new AddOperatorNode(Compile(leftside), Compile(rightside));
+                return new AddOperatorNode(this.Compile(leftside), this.Compile(rightside));
             }
 
             if (op == '-')
             {
-                return new AddOperatorNode(Compile(leftside), Compile(rightside));
+                return new AddOperatorNode(this.Compile(leftside), this.Compile(rightside));
             }
 
             if (op == '*')
             {
-                return new AddOperatorNode(Compile(leftside), Compile(rightside));
+                return new AddOperatorNode(this.Compile(leftside), this.Compile(rightside));
             }
 
             if (op == '/')
             {
-                return new AddOperatorNode(Compile(leftside), Compile(rightside));
+                return new AddOperatorNode(this.Compile(leftside), this.Compile(rightside));
             }
 
             // invalid oporaters
             throw new ArgumentException("Invalid operator: " + expression[1]);
         }
 
-        public void SetVariable(string variableName, double variableValue)
-        {
-
-        }
-
-        public double Evaluate()
-        {
-
-            return this._root.Evaluate();
-        }
-
         // since nodes don't exist outside the tree, we define them here
+
+        /// <summary>
+        /// Base abstract class for nodes.
+        /// </summary>
         public abstract class ExpressionNode
         {
+            /// <summary>
+            /// Each sublcass will have a definition.
+            /// </summary>
+            /// <returns>Value after the evaluation.</returns>
             public abstract double Evaluate();
         }
 
+        /// <summary>
+        /// Subclass for nodes with basic numbers.
+        /// </summary>
         public class NumericNode : ExpressionNode
         {
-            private double _value;
+            private double value;
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="NumericNode"/> class.
+            /// Constructor with vallue.
+            /// </summary>
+            /// <param name="value">numerical value of node.</param>
             public NumericNode(double value)
             {
-                _value = value;
+                this.value = value;
             }
 
+            /// <summary>
+            /// return the value of the node.
+            /// </summary>
+            /// <returns>numerical value.</returns>
             public override double Evaluate()
             {
-                return _value;
+                return this.value;
             }
         }
 
+        /// <summary>
+        /// Subclass for oporation nodes.
+        /// </summary>
         public abstract class OperatorNode : ExpressionNode
         {
-            protected ExpressionNode _left;
-            protected ExpressionNode _right;
+            private ExpressionNode left;
+            private ExpressionNode right;
 
+            /// <summary>
+            /// Gets for left node.
+            /// </summary>
+            protected ExpressionNode Left
+            {
+                get { return this.left; }
+            }
+
+            /// <summary>
+            /// Gets for right node.
+            /// </summary>
+            protected ExpressionNode Right
+            {
+                get { return this.right; }
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="OperatorNode"/> class.
+            /// Constructor
+            /// </summary>
+            /// <param name="left">left node.</param>
+            /// <param name="right">right node.</param>
             public OperatorNode(ExpressionNode left, ExpressionNode right)
             {
-                _left = left;
-                _right = right;
+                this.left = left;
+                this.right = right;
             }
         }
 
+        /// <summary>
+        /// Subclass for adding nodes.
+        /// </summary>
         public class AddOperatorNode : OperatorNode
         {
             public AddOperatorNode(ExpressionNode left, ExpressionNode right)
@@ -108,12 +173,19 @@ namespace SpreadsheetEngine
             {
             }
 
+            /// <summary>
+            /// Adds both sides.
+            /// </summary>
+            /// <returns>value after adding.</returns>
             public override double Evaluate()
             {
-                return _left.Evaluate() + _right.Evaluate();
+                return this.Left.Evaluate() + this.Right.Evaluate();
             }
         }
 
+        /// <summary>
+        /// Subclass for subtracting nodes.
+        /// </summary>
         public class SubtractOperatorNode : OperatorNode
         {
             public SubtractOperatorNode(ExpressionNode left, ExpressionNode right)
@@ -121,12 +193,19 @@ namespace SpreadsheetEngine
             {
             }
 
+            /// <summary>
+            /// Subtract both sides.
+            /// </summary>
+            /// <returns>value after subtracting.</returns>
             public override double Evaluate()
             {
-                return _left.Evaluate() - _right.Evaluate();
+                return this.Left.Evaluate() - this.Right.Evaluate();
             }
         }
 
+        /// <summary>
+        /// Subclass for multiplication.
+        /// </summary>
         public class MultiplyOperatorNode : OperatorNode
         {
             public MultiplyOperatorNode(ExpressionNode left, ExpressionNode right)
@@ -134,12 +213,19 @@ namespace SpreadsheetEngine
             {
             }
 
+            /// <summary>
+            /// Multiply each side
+            /// </summary>
+            /// <returns>value after multiplication</returns>
             public override double Evaluate()
             {
-                return _left.Evaluate() * _right.Evaluate();
+                return this.Left.Evaluate() * this.Right.Evaluate();
             }
         }
 
+        /// <summary>
+        /// Subclass for dividing.
+        /// </summary>
         public class DivideOperatorNode : OperatorNode
         {
             public DivideOperatorNode(ExpressionNode left, ExpressionNode right)
@@ -147,9 +233,13 @@ namespace SpreadsheetEngine
             {
             }
 
+            /// <summary>
+            /// Divide one said from another.
+            /// </summary>
+            /// <returns>value after eval.</returns>
             public override double Evaluate()
             {
-                return _left.Evaluate() / _right.Evaluate();
+                return this.Left.Evaluate() / this.Right.Evaluate();
             }
         }
     }
