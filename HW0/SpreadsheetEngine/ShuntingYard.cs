427964ab917f12e616e6a6d5d91065570dbdbd41
Nathan Bunge <nathan.bunge@wsu.edu>
2023-04-16 21:30:04 -0700

    style coup fixes for shunning yards

--- HW0/SpreadsheetEngine/ShuntingYard.cs
+++ HW0/SpreadsheetEngine/ShuntingYard.cs
@@ -1,19 +1,15 @@
-﻿using System.Runtime.CompilerServices;
+﻿// <copyright file="ShuntingYard.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
 [assembly: InternalsVisibleTo("TestExpressionTree")]
 
 namespace SpreadsheetEngine
 {
-
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Reflection;
-
-
-
-
 
     /// <summary>
     /// Will probably move to it's file.
@@ -22,73 +18,89 @@ namespace SpreadsheetEngine
     {
         private Dictionary<char, int> precedence;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShuntingYard"/> class.
+        /// Algorithm.
+        /// </summary>
+        /// <param name="operatorPrecedence">precedence dictionary. </param>
         internal ShuntingYard(Dictionary<char, int> operatorPrecedence)
         {
             this.precedence = operatorPrecedence;
-
         }
 
-        public Dictionary<char, int> Precedence { get { return this.precedence; } }
+        /// <summary>
+        /// Gets Dictinoary of predeence.
+        /// </summary>
+        public Dictionary<char, int> Precedence
+        {
+            get { return this.precedence; }
+        }
 
         /// <summary>
         /// Converts infix to postfix expression.
         /// </summary>
         /// <param name="infix">input string.</param>
-        /// <returns>postfix string</returns>
+        /// <returns>postfix string.</returns>
         /// <exception cref="ArgumentException">exception.</exception>
         public string ConvertToPostfix(string infix)
         {
             var output = new List<string>();
             var operatorStack = new Stack<char>();
-            var currentToken = "";
+            var currentToken = string.Empty;
 
             foreach (char c in infix)
             {
-                if (Char.IsLetter(c))
+                if (char.IsLetter(c))
                 {
                     currentToken += c;
                 }
-                else if (Char.IsDigit(c))
+                else if (char.IsDigit(c))
                 {
                     currentToken += c;
                 }
-                else if (IsOperator(c))
+                else if (this.IsOperator(c))
                 {
-                    if (currentToken != "")
+                    if (currentToken != string.Empty)
                     {
                         output.Add(currentToken);
-                        currentToken = "";
+                        currentToken = string.Empty;
                     }
-                    while (operatorStack.Count > 0 && IsOperator(operatorStack.Peek()) && Precedence[operatorStack.Peek()] >= Precedence[c])
+
+                    while (operatorStack.Count > 0 && this.IsOperator(operatorStack.Peek()) && this.Precedence[operatorStack.Peek()] >= this.Precedence[c])
                     {
                         output.Add(operatorStack.Pop().ToString());
                     }
+
                     operatorStack.Push(c);
                 }
                 else if (IsLeftParenthesis(c))
                 {
-                    if (currentToken != "")
+                    if (currentToken != string.Empty)
                     {
                         output.Add(currentToken);
-                        currentToken = "";
+                        currentToken = string.Empty;
                     }
+
                     operatorStack.Push(c);
                 }
                 else if (IsRightParenthesis(c))
                 {
-                    if (currentToken != "")
+                    if (currentToken != string.Empty)
                     {
                         output.Add(currentToken);
-                        currentToken = "";
+                        currentToken = string.Empty;
                     }
+
                     while (operatorStack.Count > 0 && !IsLeftParenthesis(operatorStack.Peek()))
                     {
                         output.Add(operatorStack.Pop().ToString());
                     }
+
                     if (operatorStack.Count == 0 || !IsLeftParenthesis(operatorStack.Peek()))
                     {
                         throw new ArgumentException("Unmatched parentheses in infix expression.");
                     }
+
                     operatorStack.Pop();
                 }
                 else
@@ -97,7 +109,7 @@ namespace SpreadsheetEngine
                 }
             }
 
-            if (currentToken != "")
+            if (currentToken != string.Empty)
             {
                 output.Add(currentToken);
             }
@@ -108,16 +120,11 @@ namespace SpreadsheetEngine
                 {
                     throw new ArgumentException("Unmatched parentheses in infix expression.");
                 }
+
                 output.Add(operatorStack.Pop().ToString());
             }
 
             return string.Join(" ", output);
-        }
-
-
-        private bool IsOperator(char c)
-        {
-            return this.Precedence.ContainsKey(c);
         }
 
         private static bool IsLeftParenthesis(char c)
@@ -129,6 +136,10 @@ namespace SpreadsheetEngine
         {
             return c == ')';
         }
-    }
 
+        private bool IsOperator(char c)
+        {
+            return this.Precedence.ContainsKey(c);
+        }
+    }
 }
